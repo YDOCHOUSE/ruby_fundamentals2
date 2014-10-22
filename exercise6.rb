6a4797fb6e978a437a3be1f738498916cc021786
Cody House <codyahouse@gmail.com>
2014-10-21 21:26:49 -0400

    Completed Grocery list code

---
+++ exercise6.rb
@@ -0,0 +1,26 @@
+grocery_list = ["carrots", "toilet paper", "apples", "salmon", "plastic wrap", "butcher knives"]
+
+def g_list(list)
+	list.map { |item| puts "* #{item}"}
+end
+
+puts "==== Grocery List ===="
+g_list(grocery_list)
+puts ""
+puts "The grocery list contains #{grocery_list.length} items"
+
+if grocery_list.include?("bananas")
+	puts "You don't need to pick up bananas"
+else
+	puts "You need bananas, you monkey!"
+end
+
+puts "The second Item on your list is #{grocery_list[1]}"
+puts " === Alphabetical List ==="
+sorted_list = g_list(grocery_list.sort)
+puts sorted_list
+
+grocery_list.delete("salmon")
+puts "=== Goodbye, Salmon! ==="
+g_list(grocery_list)
+
