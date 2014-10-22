991364e8ebdd73bf1154f06bccd180eab1e9bf0c
Cody House <codyahouse@gmail.com>
2014-10-22 16:17:45 -0400

    completed exercise7

--- exercise7.rb
+++ exercise7.rb
@@ -1,6 +1,29 @@
 students = {
-	cohort1: 34,
-	cohort2: 42,
-	cohort3: 22,
-	cohort4: 43
+	:cohort1 => 34,
+	:cohort2 => 42,
+	:cohort3 => 22,
 }
+
+def show_cohort(student_hash)
+	student_hash.each do |cohort, amount_students|
+		puts "#{cohort}: #{amount_students}"
+	end
+end
+
+show_cohort(students)
+
+students[:cohort4] = 43
+
+puts students.keys
+
+students.each do |key, value| 
+	students[key] = (value * 1.05).round
+end
+
+puts "#{students}"
+
+students.delete(:cohort2)
+
+updated_students = students.values.inject { |element, sum| sum + element  }
+
+puts updated_students
